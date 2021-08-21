8880fbe566971b3ee3d18585fcccec54c434ac3e
Julaxe <julaxe8@gmail.com>
2021-08-21 13:04:44 -0400

    challenge complete

--- order-summary-component/src/App.js
+++ order-summary-component/src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import imageHeader from "./images/illustration-hero.svg";
+import logoMusic from "./images/icon-music.svg";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App center">
+      <div className="content">
+        <div className="image-header">
+          <img src={imageHeader} alt="header" />
+        </div>
+        <div className="description center">
+          <h2>Order Summary</h2>
+          <p>
+            You can now listen to millions of songs, audiobooks, and podcasts on
+            any device anywhere you like!
+          </p>
+        </div>
+        <div className="plan center">
+          <div className="annual-plan center">
+            <img src={logoMusic} className="icon-music" alt="music" />
+            <div className="plan-description">
+              <h4>Annual Plan</h4>
+              <p>$59.99/year</p>
+            </div>
+            <a href="/" className="pointer">
+              Change
+            </a>
+          </div>
+        </div>
+        <div className="call-to-action center">
+          <div className="button pointer">Proceed to Payment</div>
+          <div className="cancel-order pointer">Cancel Order</div>
+        </div>
+      </div>
     </div>
   );
 }
